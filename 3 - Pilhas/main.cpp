26d8c77e795b1928bb0f236e943f46ced95e9577
Devjdias <165334469+Devjdias@users.noreply.github.com>
2025-02-10 16:27:45 -0500

    main.cpp

--- 3 - Pilhas/main.cpp
+++ 3 - Pilhas/main.cpp
@@ -16,71 +16,89 @@ int main(int argc, char* argv[]) {
 		switch(op) {
 			
 			case 1:
-				printf("\n\n\n\tOpção Criar Pilha");
+				printf("\n\n\n\tOpcao Criar Pilha");
 				inicializarPilha(&pilha);
 				printf("\n\n\nPilha inicializada com sucesso!");
 				clear();
 				break;
 			case 2:
-				printf("\n\n\n\tOpção Empilhar");
+				printf("\n\n\n\tOpcao Empilhar");
 				Livro livro;
 				livro = carregarLivro();
 				empilhar(&pilha, livro);
 				break;
 			
 			case 3:
-				printf("\n\n\n\tOpção Desempilhar");
+				printf("\n\n\n\tOpcao Desempilhar");
 				desempilhar(&pilha);
 				printf("\n\nLivro desempilhado com sucesso!\n\n");
 				clear();
 				break;
 				
 			case 4:
-				printf("\n\n\n\tOpção Exibir Pilha");
+				printf("\n\n\n\tOpcao Exibir Pilha");
 				exibirPilha(&pilha);
 				clear();
 				break;
 				
 			case 5:
-				printf("\n\n\n\tOpção Verificar - Pilha Cheia");
+				printf("\n\n\n\tOpcao Verificar - Pilha Cheia");
 				if(pilhaCheia(&pilha))
-					printf("\n\n\nA pilha está cheia!\n\n");
+					printf("\n\n\nA pilha esta cheia!\n\n");
 				else 
-					printf("\n\n\nA pilha não está cheia!\n\n");
+					printf("\n\n\nA pilha nao esta cheia!\n\n");
 				system("pause");
 				break;
 			
 			case 6:
-				printf("\n\n\n\tOpção Verificar - Pilha Vazia");
+				printf("\n\n\n\tOcao Verificar - Pilha Vazia");
 				if(pilhaVazia(&pilha))
-					printf("\n\n\nA pilha está vazia!\n\n");
+					printf("\n\n\nA pilha esta vazia!\n\n");
 				else 
-					printf("\n\n\nA pilha não está vazia!\n\n");
+					printf("\n\n\nA pilha nao esta vazia!\n\n");
 				system("pause");
 				break;
 				
 			case 7:
-				printf("\n\n\n\tOpção Exibir Topo");
+				printf("\n\n\n\tOpcao Exibir Topo");
 				Livro livroTopo;
 				livroTopo = topoPilha(&pilha);
 				printf("\n\n\tTopo da Pilha: ");
-				printf("\n\n\nCódigo do Livro: %d", livroTopo.codLivro);
+				printf("\n\n\nCodigo do Livro: %d", livroTopo.codLivro);
 				printf("\n\nNome do Livro: %s", livroTopo.nome);
 				printf("\n\nNome do Autor: %s", livroTopo.autor);
-				printf("\n\nNúmero do ISSN: %s", livroTopo.issn);
-				printf("\n\nQuantidade de Páginas: %d", livroTopo.quantPaginas);
-				printf("\n\nPreço do Livro: R$ %.2f", livroTopo.preco);
+				printf("\n\nNumero do ISSN: %s", livroTopo.issn);
+				printf("\n\nQuantidade de Paginas: %d", livroTopo.quantPaginas);
+				printf("\n\nPreco do Livro: R$ %.2f", livroTopo.preco);
 				clear();
 				break;
 			
+			case 8: {
+				printf("\n\n\n\tOpcao Buscar Livro pelo Codigo");
+				int codLivro;
+				printf("\n\nInforme o codigo do livro a ser buscado: ");
+				scanf("%d", &codLivro);
+				Livro* livroBuscado = buscarPilha(&pilha, codLivro);
+				if (livroBuscado != NULL) {
+					printf ("\n\nDados do livro encontrado: ");
+					printf("\n\n\nCodigo do Livro: %d", livroBuscado->codLivro);
+					printf("\n\nNome do Livro: %s", livroBuscado->nome);
+					printf("\n\nNome do Autor: %s", livroBuscado->autor);
+					printf("\n\nNumero do ISSN: %s", livroBuscado->issn);
+					printf("\n\nQuantidade de Paginas: %d", livroBuscado->quantPaginas);
+					printf("\n\nPreco do Livro: R$ %.2f", livroBuscado->preco);
+				} 
+				clear();
+				break;
+				}
 			case 0:
-				printf("\n\n\nVocê optou em sair!");
+				printf("\n\n\nVoce optou em sair!");
 				controle = 0;
         		clear();
 				break;
 			
 			default:
-				printf("\n\nInforme uma opção válida!");
+				printf("\n\nInforme uma opcao valida!");
 				break;	
 		}
 		
