aa35a3b1f0dcdfc4c615a09c2a0918c48b39627b
Devjdias <165334469+Devjdias@users.noreply.github.com>
2025-02-10 16:25:48 -0500

    main.cpp

--- 2 - Filas/main.cpp
+++ 2 - Filas/main.cpp
@@ -5,7 +5,7 @@
 #include "Fila.h"
 #include "menus.h"
 
-//Início da main()
+//Inicio da main()
 int main (int argc, char* argv[]) {
 	system ("cls");
 	setlocale (LC_ALL, "Portuguese");
@@ -16,13 +16,13 @@ int main (int argc, char* argv[]) {
 	inicializarFila(&fila);
 	printf ("\n\n\nEste programa manipula uma fila de cadastros de alunos!");
 	clear();
-	
-	//Laço principal do programa
+
+	//Laco principal do programa
 	do {
 		system ("cls");
 		opcao = menu();
-		
-		//Início switch()
+
+		//Inicio switch()
 		switch (opcao) {
 			case 1:
 				system ("cls");
@@ -32,9 +32,9 @@ int main (int argc, char* argv[]) {
 				printf ("\n\n\nInforme o nome do aluno: ");
 				fgets(aluno.nome, MAX, stdin);
 				aluno.nome[strcspn(aluno.nome, "\n")] = '\0';
-				printf ("\n\nInforme a matrícula do aluno: ");
+				printf ("\n\nInforme a matricula do aluno: ");
 				scanf ("%d", &aluno.matricula);
-				printf ("\n\nInforme o código do curso do aluno: ");
+				printf ("\n\nInforme o codigo do curso do aluno: ");
 				scanf ("%d", &aluno.codCurso);
 				fflush(stdin);
 				printf ("\n\nInforme o tipo do curso do aluno: ");
@@ -44,35 +44,34 @@ int main (int argc, char* argv[]) {
 				scanf ("%d", &aluno.idade);
 				printf ("\n\nInforme o coeficiente de aproveitamento do aluno: ");
 				scanf ("%f", &aluno.coefMediaGeral);
-				//Chamada da função
+				//Chamada da Funcao
 				enfileirar(&fila, aluno);
 				clear();
 				break;
-			
+
 			case 2:
 				system ("cls");
-				printf ("\n\n\nOpção Desenfileirar Cadastro de Aluno");
+				printf ("\n\n\nOpcao Desenfileirar Cadastro de Aluno");
 				if (!filaVazia(&fila)) {
 					Aluno removido = desenfileirar(&fila);
 					printf ("\n\n\nNome do aluno desenfileirado: %s.\n\n", removido.nome);
-				}
-				else
-					printf ("\n\n\nA fila de cadastros de alunos está vazia!\n\n");
+				} else
+					printf ("\n\n\nA fila de cadastros de alunos esta vazia!\n\n");
 				clear();
 				break;
-			
+
 			case 3:
 				system ("cls");
-				printf ("\n\n\nOpção Exibir Fila de Cadastros de Alunos");
+				printf ("\n\n\nOpcao Exibir Fila de Cadastros de Alunos");
 				exibirFila(&fila);
 				clear();
 				break;
 
 			case 4:
 				system ("cls");
-				printf("\n\n\nOpção desejada - Alterar Dados de Aluno na Fila");
+				printf("\n\n\nOcao desejada - Alterar Dados de Aluno na Fila");
 				if (filaVazia(&fila)) {
-				printf("\n\nA fila está vazia! Não há dados para alterar.\n");
+					printf("\n\nA fila esta vazia! Nao ha dados para alterar.\n");
 				} else {
 					printf("\n\nInforme o ID do aluno a ser alterado: ");
 					scanf("%d", &id);
@@ -81,51 +80,70 @@ int main (int argc, char* argv[]) {
 					No* atual = fila.frente;
 					int alunoEncontrado = 0;
 					while (atual != NULL) {
-							if (atual->aluno.id == id) {
-								alunoEncontrado = 1;
-								break;
-							}		
-							atual = atual->prox;
-					}	
+						if (atual->aluno.id == id) {
+							alunoEncontrado = 1;
+							break;
+						}
+						atual = atual->prox;
+					}
 					if (!alunoEncontrado) {
-							printf("\n\nAluno com ID %d não encontrado na fila.\n", id);
+						printf("\n\nAluno com ID %d nao encontrado na fila.\n", id);
 					} else {
-							printf("\n\nInforme o novo nome: ");
+						printf("\n\nInforme o novo nome: ");
 						scanf(" %[^\n]", nome);
-							printf("\n\nInforme a nova matrícula: ");
-							scanf("%d", &matricula);
-							printf("\n\nInforme o novo código do curso: ");
-							scanf("%d", &codCurso);
-							printf("\n\nInforme o novo tipo do curso: ");
-							scanf(" %[^\n]", tipoCurso);
-							printf("\n\nInforme a nova idade: ");
-							scanf("%d", &idade);
-							printf("\n\nInforme a nova média geral de notas: ");
-							scanf("%f", &coefMediaGeral);
-		
-							alterarDadosAluno(&fila, id, nome, matricula, codCurso, tipoCurso, idade, coefMediaGeral);
+						printf("\n\nInforme a nova matricula: ");
+						scanf("%d", &matricula);
+						printf("\n\nInforme o novo codigo do curso: ");
+						scanf("%d", &codCurso);
+						printf("\n\nInforme o novo tipo do curso: ");
+						scanf(" %[^\n]", tipoCurso);
+						printf("\n\nInforme a nova idade: ");
+						scanf("%d", &idade);
+						printf("\n\nInforme a nova media geral de notas: ");
+						scanf("%f", &coefMediaGeral);
+
+						alterarDadosAluno(&fila, id, nome, matricula, codCurso, tipoCurso, idade, coefMediaGeral);
 					}
 				}
-   				clear();
-		    	break;
-			
+				clear();
+				break;
+
 			case 5:
-		                system("cls");
-		                verificarRepeticoesMatricula(&fila);
-		                clear();
-               			break;
-			
+				system("cls");
+				verificarRepeticoesMatricula(&fila);
+				clear();
+				break;
+
+			case 6: {
+				system ("cls");
+				printf ("\n\n\nOpcao Buscar Cadastro de Aluno pelo ID");
+				printf ("\n\nInforme o ID do aluno a ser buscado: ");
+				scanf ("%d", &id);
+				Aluno* alunoBuscado = buscarAlunoFila(&fila, id);
+				if (alunoBuscado != NULL) {
+					printf ("\n\nDados do aluno encontrado: ");
+					printf ("\n\nID do aluno: %d", alunoBuscado->id);
+					printf ("\n\nNome do aluno: %s", alunoBuscado->nome);
+					printf ("\n\nNumero da matrícula: %d", alunoBuscado->matricula);
+					printf ("\n\nCudigo do curso: %d", alunoBuscado->codCurso);
+					printf ("\n\nTipo do curso: %s", alunoBuscado->tipoCurso);
+					printf ("\n\nIdade: %d", alunoBuscado->idade);
+					printf ("\n\nMedia geral de notas: %.2f.", alunoBuscado->coefMediaGeral);
+				}
+				clear();
+				break;
+			}
 			case 0:
 				system ("cls");
-				printf ("\n\n\nVocê optou em sair!\n\n");
+				printf ("\n\n\nVoce optou em sair!\n\n");
 				break;
-			
+
 			default:
-				printf ("\n\n\nInforme uma opção válida!");	
+				printf ("\n\n\nInforme uma opcao valida!");
 		}//Fim switch()
-		
-	} while (opcao != 0); //Fim do laço principal do programa	
-	
+
+	} while (opcao != 0); //Fim do laco principal do programa
+
 	printf ("\n\n\nObrigado por utilizar nosso programa!\n\n");
 	system ("Pause");
 	return 0;
